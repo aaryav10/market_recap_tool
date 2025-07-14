539be0e6fff440aa95157332dec48f5be55eb3e7
aaryav10 <as18794@nyu.edu>
2025-07-14 14:58:19 -0400

    Update market_recap_full_code.py

--- market_recap_full_code.py
+++ market_recap_full_code.py
@@ -18,9 +18,12 @@ import streamlit as st
 from dotenv import load_dotenv
 
 # This automatically loads the keys from your environment
-load_dotenv()
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+# load_dotenv()
+# OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+# NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+NEWS_API_KEY = st.secrets["NEWS_API_KEY"]
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 newsapi = NewsApiClient(api_key = NEWS_API_KEY)
